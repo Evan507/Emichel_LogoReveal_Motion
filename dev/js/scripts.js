a54320073097b7117ac0e2b2677b262dc3d3442b
Evan <81766815+Evan507@users.noreply.github.com>
2022-05-31 23:29:32 -0400

    basic SplashEffects Implemented

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -26,23 +26,32 @@ logoanim
 .from("#BirdFlying", {x: -300, y: -500, duration:4}, "arrival")
 .to("#BirdFlying", {delay:0, duration:0.05, opacity: 0}, "standup")
 .from("#Bird" ,{delay:0, duration:0.05, opacity: 0}, "standup")
-.to("#Bird", {x:500, duration: 7, ease: "back.out(1.7)"}, "moveRight1")
-.to("#Helmet", {x:500, duration: 7, ease: "back.out(1.7)"}, "moveRight1")
-.to("#Bird", {x:0, duration: 7, ease: "back.out(1.7)"}, "moveLeft1")
-.to("#Helmet", {x:0, duration: 7, ease: "back.out(1.7)"}, "moveLeft1")
-.to("#Bird", {x:250, duration: 7, ease: "back.out(1.7)"}, "moveRight2")
-.to("#Helmet", {x:250, duration: 7, ease: "back.out(1.7)"}, "moveRight2")
+// .to("#eye", {delay:3, duration: 2, morphSVG:"#blink"}, "blink1")
+.to("#Bird", {delay:0, x:500, duration: 13, ease: "back.out(1)"}, "moveRight1")
+.to("#Helmet", {delay:0, x:500, duration: 13, ease: "back.out(1)"}, "moveRight1")
+.from("#Splash1", {delay:0.2, opacity:0}, "moveRight1" )
+.to("#Splash1", {delay:1, opacity:0}, "moveRight1")
+.from("#Splash2", {delay: 4, opacity:0}, "moveRight1" )
+.to("#Splash2", {delay:5, opacity:0}, "moveRight1")
+.to("#Bird", {delay:0, x:0, duration: 13, ease: "back.out(1)"}, "moveLeft1")
+.to("#Helmet", {delay:0, x:0, duration: 13, ease: "back.out(1)"}, "moveLeft1")
+.from("#Splash3", {delay:1.5, opacity:0}, "moveLeft1" )
+.to("#Splash3", {delay:2, opacity:0}, "moveLeft1")
+.to("#Bird", {delay:0, x:250, duration: 7, ease: "back.out(1)"}, "moveRight2")
+.to("#Helmet", {delay:0, x:250, duration: 7, ease: "back.out(1)"}, "moveRight2")
+.to("#Bird", {delay:0, y:-250, duration: 7}, "moveUp")
+.to("#Helmet", {delay:0, y:-250, duration: 7}, "moveUp")
 
 
-.from("#BirdFlying2", {opacity:0})
 
-
-
-
+.from("#flyaway", {opacity:0})
 
 
 
 ;
+
+
+
 
 
 
